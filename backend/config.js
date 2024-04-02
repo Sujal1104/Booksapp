ac58ef76e9ed0bf831ea08250b7bb8990bb7775a
sujal <sujalsujal73838@gmail.com>
2024-04-02 17:21:31 +0530

    book

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://21it079:B8sknbp1So7HMFOh@cluster0.i2dkh3m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
